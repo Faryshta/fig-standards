8f474fe4c4d1a0716a339b3039e3c6e6ef6f0c6c
Faryshta <angeldelcaos@gmail.com>
2013-02-13 13:01:20 -0600

    create Rule Interface

---
+++ proposed/FormValidator/Rule.php
@@ -0,0 +1,32 @@
+<?php
+
+interface Rule{
+    /**
+     * Sanitize user input
+     *
+     * Example: turning 1 234 to 1234
+     *
+     * @param string $value
+     * @return string
+     */
+    public sanitize($value);
+
+    /**
+     * Validate sanitized input
+     *
+     * @param string $value
+     * @throw RuntimeException
+     */
+    public validate($value);
+
+    /**
+     * Takes a value to display to the user
+     *
+     * Example: turning 1234 to 1 234.00
+     * encrypting emails
+     *
+     * @param string $value
+     * @return string
+     */
+    public draw($value);
+}
